00508bc83da15fa32e834587833362f69564006f
Daniel <33708658+danieljhkim@users.noreply.github.com>
2026-04-02 00:16:40 -0700

    fix: add explicit version, use pre-built binary install

--- Formula/orbit.rb
+++ Formula/orbit.rb
@@ -3,19 +3,14 @@ class Orbit < Formula
   homepage "https://github.com/danieljhkim/orbit"
   url "https://github.com/danieljhkim/orbit/releases/download/v0.1.0/orbit_0.1.0_darwin_arm64.tar.gz"
   sha256 "bcae20e98e5e35c796c1b37d1b10e86bdc899576060e51b193cd9aeac6589ba7"
+  version "0.1.0"
   license "MIT"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "install", *std_cargo_args(path: "orbit-cli")
+    bin.install "orbit"
   end
 
   test do
-    assert_match "orbit #{version}", shell_output("#{bin}/orbit --version")
-
-    # Verify init creates the global root
-    system bin/"orbit", "init", "--root", testpath/".orbit"
-    assert_predicate testpath/".orbit/config.toml", :exist?
+    assert_match "orbit", shell_output("#{bin}/orbit --version")
   end
 end
