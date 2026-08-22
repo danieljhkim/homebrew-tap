bcf8d4073c98e352559ff330a144856d4a641054
danieljhkim <danieljhkim1@gmail.com>
2026-08-22 16:47:36 -0700

    monodev v0.2.8

--- Formula/monodev.rb
+++ Formula/monodev.rb
@@ -1,9 +1,9 @@
 class Monodev < Formula
   desc "Local-only CLI for managing reusable dev overlays across large monorepos"
   homepage "https://github.com/danieljhkim/monodev"
-  url "https://github.com/danieljhkim/monodev/releases/download/v0.2.7/monodev_0.2.7_darwin_arm64.tar.gz"
-  sha256 "9441861ee64b436f0d8028cb3adef730e3dc72aa1fdc595951fa67df0d0351d2"
-  version "0.2.7"
+  url "https://github.com/danieljhkim/monodev/releases/download/v0.2.8/monodev_0.2.8_darwin_arm64.tar.gz"
+  sha256 "5a3ff3e242b165f1154c4ce251d5876321f3f57bdb231c0ec49467bb63e5c768"
+  version "0.2.8"
 
   def install
     bin.install "monodev"
