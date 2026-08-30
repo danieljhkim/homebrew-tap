ecc89116755c1bf8e1b784c2b375a4e984dca141
danieljhkim <danieljhkim1@gmail.com>
2026-08-30 01:00:18 -0700

    local-data v0.4.0

--- Formula/local-data.rb
+++ Formula/local-data.rb
@@ -1,15 +1,21 @@
 class LocalData < Formula
   desc "Local Hadoop + Hive + Spark development environment for macOS"
   homepage "https://github.com/danieljhkim/local-data-platform"
-  url "https://github.com/danieljhkim/local-data-platform/releases/download/v0.3.3/local-data_0.3.3_darwin_arm64.tar.gz"
-  sha256 "75c738c0d0e2eccd3d2a839b5f9c1aaf7d212f28371950ef475215660135862c"
+  version "0.4.0"
   license "MIT"
-  version "0.3.3"
 
-  depends_on "openjdk@17"
+  if Hardware::CPU.arm?
+    url "https://github.com/danieljhkim/local-data-platform/releases/download/v0.4.0/local-data_0.4.0_darwin_arm64.tar.gz"
+    sha256 "54e9bb35f5537b91117875aef4d92b9d825cab948b2262570a34af54f1e226fa"
+  else
+    url "https://github.com/danieljhkim/local-data-platform/releases/download/v0.4.0/local-data_0.4.0_darwin_amd64.tar.gz"
+    sha256 "12697f7e733ee9804385aa6f968cf7d43a2de43afb2613844663c2b4bd38d045"
+  end
+
+  depends_on "apache-spark"
   depends_on "hadoop"
   depends_on "hive"
-  depends_on "apache-spark"
+  depends_on "openjdk@17"
 
   def install
     bin.install "local-data"
